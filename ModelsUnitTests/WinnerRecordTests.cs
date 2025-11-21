0b20eafad795aab26aee68f691fd1beaf6e22751
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2025-11-21 12:57:01 +0100

    createdModel

---
+++ ModelsUnitTests/WinnerRecordTests.cs
@@ -0,0 +1,25 @@
+using Projekt.Models;
+
+namespace ModelsUnitTests
+{
+    public class WinnerRecordTests
+    {
+        [Test]
+        [TestCaseSource(nameof(_Records))]
+        public void Constructor_CreateObject_CorrectValuesForAttributes(string name, TimeSpan time, DateTime date)
+        {
+            WinnerRecord record = new(name, time, date);
+            Assert.Multiple(() => {
+                Assert.That(record.Name, Is.EqualTo(name));
+                Assert.That(record.Time, Is.EqualTo(time));
+                Assert.That(record.AchievedAt, Is.EqualTo(date));
+            });
+        }
+        private static object[] _Records =
+        {
+            new object[] { "winner", new TimeSpan(0, 0, 50 ), new DateTime(2025, 11, 20) },
+            new object[] { "steven", new TimeSpan(0, 1, 50 ), new DateTime(2025, 11, 21) },
+            new object[] { "seb", new TimeSpan(10, 20, 50 ), new DateTime(2025, 11, 30) }
+        };
+    }
+}
---
+++ RestAPI/Models/WinnerRecord.cs
@@ -0,0 +1,17 @@
+﻿namespace Projekt.Models
+{
+    public class WinnerRecord
+    {
+        public int ID;
+        public string Name { get; }
+        public  TimeSpan Time { get; }
+        public DateTime AchievedAt { get; }
+
+        public WinnerRecord(string name, TimeSpan time, DateTime date) 
+        {
+            Name = name;
+            Time = time;
+            AchievedAt = date;
+        }
+    }
+}
